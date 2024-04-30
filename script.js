e2e85632bca085cef492c7c07040623c73f645f3
aeviloria93 <134814106+aeviloria93@users.noreply.github.com>
2024-04-30 20:15:42 -0300

    feat: Cambio de API y constantes pag principal

--- script.js
+++ script.js
@@ -2,7 +2,39 @@ const url_pokemons = 'https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20' ;
 
 //console.log(url_pokemons);
 
-fetch(url_pokemons).then(lista => lista.json())
+//https://pokeapi.co/api/v2/ability
+
+const url_pokemons2 = 'https://pokeapi.co/api/v2/pokemon/1'
+
+
+
+fetch(url_pokemons2).then(lista => lista.json())
+.then(resultado => {
+    console.log("result", resultado)
+
+    const nombre = resultado["name"]
+    console.log(nombre)
+    const img = resultado.sprites.front_default
+    console.log(img)
+    
+   
+    
+
+
+    //const primerPokemon = pokemons[0];
+    //console.log(primerPokemon);
+    
+   // const poke_detalle = primerPokemon.url ;
+    //console.log(poke_detalle)
+    
+})
+
+
+
+
+
+
+/* fetch(url_pokemons).then(lista => lista.json())
 .then(resultado => {
     const pokemons = resultado.results;
     console.log(pokemons)
@@ -13,7 +45,7 @@ fetch(url_pokemons).then(lista => lista.json())
     const poke_detalle = primerPokemon.url ;
     //console.log(poke_detalle)
     
-})
+}) */
 
 
 
