c00169cc7e0c9b5658093dda9118f8bda749ed7f
aeviloria93 <134814106+aeviloria93@users.noreply.github.com>
2024-04-30 19:04:49 -0300

    feat: Accediendo a la pripiedad url del un pokemon

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const url_pokemons = 'https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20' ;
 
-console.log(url_pokemons);
+//console.log(url_pokemons);
 
 fetch(url_pokemons).then(lista => lista.json())
 .then(resultado => {
-    const pokemons = resultado.results
+    const pokemons = resultado.results;
     console.log(pokemons)
+    
+    const primerPokemon = pokemons[0];
+    //console.log(primerPokemon);
+    
+    const poke_detalle = primerPokemon.url ;
+    //console.log(poke_detalle)
+    
 })
+
+
+
+
+
